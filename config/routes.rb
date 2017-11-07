beb9640ec672a7529769b18a63972498987cd692
Jonathan Yiv <JonathanYiv@gmail.com>
2017-11-06 20:35:21 -0500

    Got registration form working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations'}
   root 'static_pages#home'
   
   get '/contact', to: 'static_pages#contact'
