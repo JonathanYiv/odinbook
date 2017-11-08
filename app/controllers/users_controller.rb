a921e18d4315270eb055c520bb2c5ee8a1a2b515
Jonathan Yiv <JonathanYiv@gmail.com>
2017-11-08 18:55:03 -0500

    Finished most of the profile page, added links directing towards users#show action across the site

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   before_action :logged_in_user
-  
+
   def index
     @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   def edit
