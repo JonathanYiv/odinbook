5c21ba6bdb68a35edd20f42df732439ab8fb4e7b
Jonathan Yiv <JonathanYiv@gmail.com>
2017-11-09 05:08:48 -0500

    Preliminary database seeding -- dear God, pagination and getting rid of N+1 queries is important

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Creates my Master Account
+super_user = User.create!(email: "JonathanYiv@gmail.com",
+                          password: "password",
+                          bio: "I am the Dragon",
+                          first_name: "Jonathan",
+                          last_name: "Yiv")
+
+# Create 100 Seed Accounts
+100.times do |n|
+  email       = Faker::Internet.email
+  password    = "password"
+  bio         = Faker::Lorem.sentence
+  first_name  = Faker::Name.first_name
+  last_name   = Faker::Name.last_name
+
+  User.create!(email:      email,
+               password:   password,
+               bio:        bio,
+               first_name: first_name,
+               last_name: last_name)
+end
+
+# Grabs all the Users
+users = User.all
+
+# All users establish friendships with my master account
+users.each do |user|
+  Friendship.create(requester: user,
+                    requested: super_user,
+                    accepted: true)
+end
+
+# Grabs the 10 last created users
+recent_users = users.order(created_at: :desc).take(10)
+
+# Creates 50 Posts for each of these users
+50.times do
+  content = Faker::Lorem.sentences(2).join(" ")
+  users.each { |user| user.posts.create!(content: content) }
+end
+
+# Grabs the 90 other users
+other_users = users.order(created_at: :asc).take(90)
+
+# Creates 5 Posts for the remaining users
+5.times do
+  content = Faker::Lorem.sentences(2).join(" ")
+  users.each { |user| user.posts.create!(content: content) }
+end
+
+# Need to create comments, likes, friendship requests, and friendships
